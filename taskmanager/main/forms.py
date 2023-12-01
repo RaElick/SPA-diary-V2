a41a2e9229b2c60c13e37dda3073aa1089419882
kikitooo <nik.puchok@gmail.com>
2023-12-01 21:13:57 +0300

    Изменена форма для создания заметок, добавлено поле ввода даты

--- taskmanager/main/forms.py
+++ taskmanager/main/forms.py
@@ -1,11 +1,11 @@
 from .models import Task
-from django.forms import ModelForm, TextInput, Textarea
+from django.forms import ModelForm, TextInput, Textarea, DateTimeInput
 
 
 class TaskForm(ModelForm):
     class Meta:
         model=Task
-        fields = ["title", "task"]
+        fields = ["title", "task", "date"]
         widgets = {
             "title" : TextInput(attrs={
                 'class':'form-control',
@@ -15,5 +15,9 @@ class TaskForm(ModelForm):
                 'class':'form-control',
                 'placeholder': 'Введите описание'
             }),
+            "date": DateTimeInput(attrs={
+                'class': 'datetime-input',
+                'placeholder': 'Дата заметки'
+            })
         }
 
--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField('Название', max_length=50)
     task = models.TextField('Описание')
+    date = models.DateTimeField('Дата Заметки')
 
     def __str__(self):
         return self.title
